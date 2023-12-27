582b777008162db3e5ff37962a105b163fa8e526
anacorreia15 <ana.correia2@ipcbcampus.pt>
2023-12-27 11:48:48 +0000

    First commit

---
+++ src/main/java/com/AD/microservicotarefas/MicroservicoTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.AD.microservicotarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicoTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicoTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AD/microservicotarefas/MicroservicoTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AD.microservicotarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicoTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
